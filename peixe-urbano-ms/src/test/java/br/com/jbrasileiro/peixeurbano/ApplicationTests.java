7d8cff5fb04aecfb3c1aac723196f616d14da183
João Carlos Brasileiro <j.brasileiro@sap.com>
2019-10-12 03:15:03 -0300

    add test

--- peixe-urbano-ms/src/test/java/br/com/jbrasileiro/peixeurbano/ApplicationTests.java
+++ peixe-urbano-ms/src/test/java/br/com/jbrasileiro/peixeurbano/ApplicationTests.java
@@ -1,7 +1,9 @@
 package br.com.jbrasileiro.peixeurbano;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -9,8 +11,24 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class ApplicationTests {
 
+	@Value("${value}")
+	private String value;
+	@Value("${application-profile}")
+	private String profile;
+
 	@Test
 	public void contextLoads() {
 	}
 
+	@Test
+	public void value()
+		throws Exception {
+		Assert.assertEquals("production", value);
+	}
+
+	@Test
+	public void profile()
+		throws Exception {
+		Assert.assertEquals("production", profile);
+	}
 }
